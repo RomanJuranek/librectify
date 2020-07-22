54cf956c1849547e514326da06e4ce2a1d710c3a
Roman Juranek <ijuranek@fit.vutbr.cz>
2020-07-22 14:26:30 +0200

    test update

--- src/test.cpp
+++ src/test.cpp
@@ -15,26 +15,26 @@ ostream & operator<<(ostream & out, const Point & p)
 
 int main()
 {
-    LineSegment ls[4];
-    ls[2] = {0,-1, 1,0, 1,0, 10}; 
-    ls[3] = {0, 1, 1,0, 1,0, 10};
 
-    ls[0] = {0,-1,-1,0, 1,0, 0};
-    ls[1] = {0, 1,-1,0, 1,0, 0};
+    LineSegment A = {0, 0, 10,0, 1,0, 10};
+    LineSegment B = {10,0, 8,5, 1,0, 1};
+    LineSegment C = {8,5, 2,5, 1,0, 10};
+    LineSegment D = {2,5, 0,0, 1,0, 1};
 
+    LineSegment ls[4] = {A,B,C,D};
     int n_lines = 4;
 
-    Point vp1 = fit_vanishing_point(ls, n_lines, 0);
+    Point vp1 = fit_vanishing_point(ls, n_lines, 1);
     cout << "vp1=" << vp1 << endl;
 
-    Point vp2 = fit_vanishing_point(ls, n_lines, 1);
+    Point vp2 = fit_vanishing_point(ls, n_lines, 10);
     cout << "vp2=" << vp2 << endl;
 
     Point vp3 = fit_vanishing_point(ls, n_lines, -1);
     cout << "vp3=" << vp3 << endl;
 
 
-    LineSegment line = {-1,-1, 1, 0, 1,0, -1};
+    LineSegment line = {5,1, 5,4, 1,0, -1};
 
     assign_to_group(ls, n_lines, &line, 1, 10);
 
