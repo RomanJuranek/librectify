c7d4fc2c8dda12dfb0a5f50b267e723efd567af9
Roman Juranek <ijuranek@fit.vutbr.cz>
2020-07-23 13:32:15 +0200

    Parameter tuning

--- src/config.h
+++ src/config.h
@@ -18,25 +18,25 @@ using InputPixelType = float;
 ///////////////////////////////////////////////////////////////////////////////
 // LINE GROUPS
 
-// The maximal number of groups to detect. Usually 2 to 4 groups have useful
-// geometric meaning.
-#define MAX_GROUPS 4
+// // The maximal number of groups to detect. Usually 2 to 4 groups have useful
+// // geometric meaning.
+// #define MAX_GROUPS 4
 
-// Minimum number of lines to start RANSAC/PROSAC
-#define MIN_LINES 10
+// // Minimum number of lines to start RANSAC/PROSAC
+// #define MIN_LINES 10
 
-// Minimum weight (cos of inclination to the VP) for a line to be considered
-// inlier (after RANSAC)
-#define INLIER_TOLERANCE 3e-4
+// // Minimum weight (cos of inclination to the VP) for a line to be considered
+// // inlier (after RANSAC)
+// #define INLIER_TOLERANCE 3e-4
 
-// RANSAC
-// Number of iterations of RANSAC
-#define RANSAC_MAX_ITER 10000
+// // RANSAC
+// // Number of iterations of RANSAC
+// #define RANSAC_MAX_ITER 10000
 
-// PROSAC
-#define PROSAC_BETA 0.1f
-#define PROSAC_PHI 0.05f
-#define PROSAC_ETHA 0.05f
+// // PROSAC
+// #define PROSAC_BETA 0.1f
+// #define PROSAC_PHI 0.05f
+// #define PROSAC_ETHA 0.05f
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -52,16 +52,16 @@ using InputPixelType = float;
 #define SEED_DIST 2
 
 // The ration of seed points to consider for tracing
-#define SEED_RATIO 0.9
+#define SEED_RATIO 0.95
 
 // Magnitude tolerance during tracing
 #define TRACE_TOLERANCE 0.3
 
 // Mean reprojection error of pixels on a line
-#define LINE_MAX_ERR 1.5f
+#define LINE_MAX_ERR 1.7f
 
 // Mean weight of pixels on a line
-#define LINE_MIN_WEIGHT 0.02f
+#define LINE_MIN_WEIGHT 0.01f
 
 // Minimal line length
 #define LINE_MIN_LENGTH 10.f
