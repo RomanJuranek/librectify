0e1e133805174ab549ffa8091ba241bcddad064b
Roman Juranek <ijuranek@fit.vutbr.cz>
2020-08-05 12:24:55 +0200

    parameter tuning

--- src/config.h
+++ src/config.h
@@ -52,19 +52,19 @@ using InputPixelType = float;
 #define SEED_DIST 2
 
 // The ration of seed points to consider for tracing
-#define SEED_RATIO 0.95
+#define SEED_RATIO 0.9
 
 // Magnitude tolerance during tracing
 #define TRACE_TOLERANCE 0.3
 
 // Mean reprojection error of pixels on a line
-#define LINE_MAX_ERR 1.7f
+#define LINE_MAX_ERR 1.5f
 
 // Mean weight of pixels on a line
-#define LINE_MIN_WEIGHT 0.01f
+#define LINE_MIN_WEIGHT 0.02f
 
 // Minimal line length
-#define LINE_MIN_LENGTH 10.f
+#define LINE_MIN_LENGTH 5.f
 
 // Minimal number of pixel of image component for line fitting
-#define COMPONENT_MIN_SIZE 10
+#define COMPONENT_MIN_SIZE 5
